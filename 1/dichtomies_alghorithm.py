755792726ecfa6fe085bfa73b244d774e50cfd27
iskander-faggod <kudashev.iskander13@gmail.com>
2022-03-13 00:53:38 +0300

    fix: misha updates

--- 1/dichtomies_alghorithm.py
+++ 1/dichtomies_alghorithm.py
@@ -1,12 +1,6 @@
 import math
 
 
-def func(x: float, eps=1e-5) -> float:
-    if x == 0:
-        x = eps
-    return math.sqrt(x + 1) - 1 / x
-
-
 def dichotomies_algorithm(function: (float, float), a: int, b: int, eps=1e-5):
     n = 0
     while abs(b - a) > eps:
@@ -20,9 +14,3 @@ def dichotomies_algorithm(function: (float, float), a: int, b: int, eps=1e-5):
         else:
             b = x
     return x, n
-
-
-x, n = dichotomies_algorithm(func, 0, 1)
-print(x)
-print(n)
-print(func(x))
