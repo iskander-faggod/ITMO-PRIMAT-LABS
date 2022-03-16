af2e48042723d29a965468c6cd9b5f19c23f5f7c
iskander-faggod <kudashev.iskander13@gmail.com>
2022-03-16 22:59:51 +0300

    fix: change a,b type + x check

--- 1/dichtomies_alghorithm.py
+++ 1/dichtomies_alghorithm.py
@@ -1,11 +1,13 @@
 import math
 
 
-def dichotomies_algorithm(function: (float, float), a: int, b: int, eps=1e-5):
+def dichotomies_algorithm(function: (float, float), a: float, b: float, eps=1e-5):
     n = 0
     while abs(b - a) > eps:
         n += 1
         x = (a + b) / 2.0
+        if x == 0:
+            print("x равен нулю ", x)
         function_with_x_result = function(x)
         function_with_a_result = function(a)
         if (function_with_x_result < 0 and function_with_a_result < 0) \
