d684afe274547af168afb4db2f1476f12d339e98
iskander-faggod <kudashev.iskander13@gmail.com>
2022-03-29 18:39:02 +0300

    fix: return value + eps to eps/2

--- 1/dichtomies_alghorithm.py
+++ 1/dichtomies_alghorithm.py
@@ -1,9 +1,8 @@
 from math import sin, cos, log
 import matplotlib.pyplot as plt
 
-
 eps = 0.001  # точность
-f = lambda x: x**2 + 3   # функция
+f = lambda x: x ** 2 + 3  # функция
 a = -1  # левая граница
 b = 1  # правая граница
 
@@ -12,15 +11,19 @@ plt.plot([i / 1000 + a for i in range((b - a) * 1000)],
          [f(i / 1000 + a) for i in range((b - a) * 1000)])
 plt.show()
 
+
 def dichtomy(a: float, b: float, eps: float):
-    iteration_counter : int = 0
+    interval_sizes = []
+    iteration_counter: int = 0
     while abs(a - b) > eps:
+        interval_sizes.append(a - b)
         iteration_counter += 1
         c = (a + b) / 2
-        if f(c - eps) <= f(c + eps):
+        if f(c - (eps / 2)) <= f(c + (eps / 2)):
             b = c
         else:
             a = c
-    return c, iteration_counter, c*2
+    return c, iteration_counter, iteration_counter * 2, interval_sizes
 
-print(dichtomy(a,b,eps))
+
+print(dichtomy(a, b, eps))
